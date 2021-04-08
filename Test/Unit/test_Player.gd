5edda34babc580ad9299420607a369485a73ed56
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-08 19:31:03 +0200

    Removed orphan warning

--- Test/Unit/test_Player.gd
+++ Test/Unit/test_Player.gd
@@ -1,12 +1,20 @@
 extends 'res://addons/gut/test.gd'
 
 var Player = load("res://Player/Player.gd");
+var _player = null;
 
+func before_each():
+	_player = Player.new();
+	
+	
+func after_each():
+	_player.free();
+	
+	
 func test_some_method():
 	
-	var _player = Player.new()
+	
 	var result = _player.some_method()
-	pass # Replace with function body.
 	
 	assert_eq(result, "bananas", "The result should have been bananas");
 
