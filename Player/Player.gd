a24109b8e2bb6029ee5267856e852fd391810884
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-12 17:03:00 +0200

    Extracted movement to move_state()

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,13 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -14,6 +21,10 @@ func _ready():
 	animationTree.active = true
 
 func _physics_process(delta):
+	move_state(delta)
+
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -29,6 +40,7 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
 
 
 ############### TDD example ###############
