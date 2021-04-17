1d2937cec2ef2f1bb7efda69a6a13334921a9de4
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-17 15:48:32 +0200

    Bats face the direction they are moving. Placed the bats more properly in the world.

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -17,6 +17,7 @@ var knockback = Vector2.ZERO
 
 var state = CHASE
 
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 
@@ -37,6 +38,7 @@ func _physics_process(delta):
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
 				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				sprite.flip_h = velocity.x < 0
 				
 	velocity = move_and_slide(velocity)
 	
