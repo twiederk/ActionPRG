1b5e9e059b505d45016d6c09d8fdd44f2ca16fe1
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-19 07:06:49 +0200

    Replaced label with full and empty hearts

--- UI/HealthUI.gd
+++ UI/HealthUI.gd
@@ -4,12 +4,9 @@ extends Control
 var hearts = 4 setget set_hearts
 var max_hearts = 4 setget set_max_hearts
 
-onready var label = $Label
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
-	if label != null:
-		label.text = "HP = " + str(hearts)
 
 func set_max_hearts(value):
 	max_hearts = max(value, 1)
