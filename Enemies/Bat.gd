e1a2e4d0b41d9a656f28fbf4feb8a870c86fd5e9
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-19 17:18:50 +0200

    Added soft collision to bats so they don't overlap each other

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -21,6 +21,7 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
+onready var softCollision = $SoftCollision
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -42,6 +43,9 @@ func _physics_process(delta):
 				sprite.flip_h = velocity.x < 0
 			else:
 				state = IDLE
+				
+	if softCollision.is_colliding():
+		velocity = softCollision.get_push_vector() * delta * 400
 				
 	velocity = move_and_slide(velocity)
 	
