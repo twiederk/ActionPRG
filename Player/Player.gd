239844af69a680bf4aa43cecf8df42e49fbcf465
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-21 16:09:19 +0200

    Added blinking to bats

--- Player/Player.gd
+++ Player/Player.gd
@@ -94,7 +94,7 @@ func attack_animation_finished():
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= 1
-	hurtbox.start_invincibility(0.5)
+	hurtbox.start_invincibility(0.6)
 	hurtbox.create_hit_effect()
 	var playerHurtSound = PlayerHurtSound.instance()
 	get_tree().current_scene.add_child(playerHurtSound)
