61c0d43f087c053eb6ab24797e86e8c629b21d48
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-04-24 04:49:27 +0200

    Display keyboard on Android only

--- UI/Keyboard.gd
+++ UI/Keyboard.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-var showKeyboard = true
+export(bool) var showKeyboard = false
+
+func _ready():
+	if OS.get_name() == "Android":
+		showKeyboard = true
+	set_visible(showKeyboard)
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("keyboard"):
