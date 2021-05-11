cc92a1142fa7bb783d4efbadb9620657f7b28ecf
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-05-11 06:26:04 +0200

    Disable collision with door if key is found, which looks much nicer

--- World/Door.gd
+++ World/Door.gd
@@ -4,8 +4,8 @@ extends Node2D
 onready var collisionShape = $StaticBody2D/CollisionShape2D
 
 
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(body : KinematicBody2D) -> void:
 	if PlayerStats.key_gold == true:
-		queue_free()
+		collisionShape.set_deferred("disabled", true)
 
 
