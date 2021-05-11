10d20e93fd9df9a14d1e86f1e97730b7d5f2d18b
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-05-11 06:18:47 +0200

    Remove door with key

--- World/Door.gd
+++ World/Door.gd
@@ -1,7 +1,11 @@
 class_name Door
 extends Node2D
 
-onready var collisionShape = $CollisionShape2D
-onready var keyCollisionShape = $KeyCollisionShape2D
+onready var collisionShape = $StaticBody2D/CollisionShape2D
+
+
+func _on_Area2D_body_entered(body):
+	if PlayerStats.key_gold == true:
+		queue_free()
 
 
