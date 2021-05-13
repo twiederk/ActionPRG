2ebcaea1d8ae72cebe8193359e2a6d4962a08694
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-05-13 04:55:26 +0200

    Fix bug with entered_healing_area signal

--- World/HealingWell.gd
+++ World/HealingWell.gd
@@ -1,8 +1,8 @@
 class_name HealingWell
-extends StaticBody2D
+extends Area2D
 
 signal entered_healing_area
 
-func _on_HealingArea2D_body_entered(_body):
+func _on_HealingWell_body_entered(body):
 	emit_signal("entered_healing_area")
 	
