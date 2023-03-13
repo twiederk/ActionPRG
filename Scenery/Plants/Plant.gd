cda69314e199ef3f9c46806eb382514522b4702e
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-03-13 11:11:14 +0100

    Renamed World to Scenery

--- Scenery/Plants/Plant.gd
+++ Scenery/Plants/Plant.gd
@@ -1,7 +1,7 @@
 class_name Plant
 extends StaticBody2D
 
-export(Resource) var plant_resource = preload("res://World/Plants/Tree.tres")
+export(Resource) var plant_resource = preload("res://Scenery/Plants/Tree.tres")
 
 onready var sprite = $Sprite
 
