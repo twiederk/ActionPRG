81cb7925417017c694761aa0f321e485d01dcfe1
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-04-06 07:00:54 +0200

    Removed print debug statement

--- Levels/Village/Village.gd
+++ Levels/Village/Village.gd
@@ -8,6 +8,8 @@ const NAME = "Village"
 
 
 func _on_NormalDoor_door_opened(world_position):
-	print("open the door")
 	var door_position = wall_tile_map.local_to_map(world_position)
 	wall_tile_map.set_cellv(door_position, -1)
+#	tilemap.set_cell(TILE_SET_LAYER_GROUND, map_position, TILE_SET_SOURCE_ID, TILE_GRASS)
+#	wall_tile_map.set_cell(1, door_position, 0, -1)
+
