7067755c18501394aae7165c90ceb779be8ca995
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-04-06 18:54:00 +0200

    Fixed _on_NormalDoor_door_opend

--- Levels/Village/Village.gd
+++ Levels/Village/Village.gd
@@ -3,13 +3,12 @@ extends Node
 
 const STARTING_POSITION = Vector2(495, 152)
 const NAME = "Village"
+const WALL_LAYER = 0
 
 @onready var wall_tile_map = $WallTileMap4
 
 
 func _on_NormalDoor_door_opened(world_position):
 	var door_position = wall_tile_map.local_to_map(world_position)
-	wall_tile_map.set_cellv(door_position, -1)
-#	tilemap.set_cell(TILE_SET_LAYER_GROUND, map_position, TILE_SET_SOURCE_ID, TILE_GRASS)
-#	wall_tile_map.set_cell(1, door_position, 0, -1)
+	wall_tile_map.erase_cell(WALL_LAYER, door_position)
 
