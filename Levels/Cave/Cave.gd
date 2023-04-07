6fee1257ba5a7ce4ce4fae5e071b95829ebaff71
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-04-07 06:07:13 +0200

    Painted floor below door of Wizard Tower

--- Levels/Cave/Cave.gd
+++ Levels/Cave/Cave.gd
@@ -5,4 +5,4 @@ extends Node
 
 func _on_NormalDoor_door_opened(world_position):
 	var door_position = fence_tile_map.local_to_map(world_position)
-	fence_tile_map.set_cellv(door_position, -1)
+	fence_tile_map.erase_cell(0, door_position)
