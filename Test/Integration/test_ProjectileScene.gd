0dc55dab8401a6fd25042a2fc234f440961337d0
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-04-13 05:14:56 +0200

    Fixed test_ProjectileScene

--- Test/Integration/test_ProjectileScene.gd
+++ Test/Integration/test_ProjectileScene.gd
@@ -15,7 +15,8 @@ func test_ready():
 	add_child(projectile_scene)
 	
 	# assert
-	assert_eq(projectile_scene.sprite.frame_coords, Vector2(10, 20), "Should set frame coords of ranged weapon")
+	assert_eq(projectile_scene.sprite.frame_coords, Vector2i(10, 20), "Should set frame coords of ranged weapon")
+#	assert_eq(projectile_scene.sprite.frame_coords, Vector2i(10, 20))
 
 	# tear down
 	for child in get_children():
