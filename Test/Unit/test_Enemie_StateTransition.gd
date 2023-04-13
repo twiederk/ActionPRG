939876c787e976c92a4bd463e6ad1963debef4a2
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2023-04-13 06:21:23 +0200

    Fixed test_Enemie_StateTransition

--- Test/Unit/test_Enemie_StateTransition.gd
+++ Test/Unit/test_Enemie_StateTransition.gd
@@ -48,8 +48,10 @@ func test_shoot_state_player_null_IDLE():
 func test_shoot_state_player_in_chase_zone_CHASE():
 
 	# arrange
-	enemie.shootDetectionZone = stub_detection_zone(true)
-	enemie.chaseDetectionZone = stub_detection_zone(true)
+	enemie.shootDetectionZone = PlayerDetectionZone.new()
+	enemie.shootDetectionZone.player = Node2D.new()
+	enemie.chaseDetectionZone = PlayerDetectionZone.new()
+	enemie.chaseDetectionZone.player = Node2D.new()
 
 	# act
 	enemie.shoot_state(1.0)
@@ -58,7 +60,10 @@ func test_shoot_state_player_in_chase_zone_CHASE():
 	assert_eq(enemie.state, Enemie.EnemieState.CHASE, "Should set state to CHASE when no player is in RangedWeaponDetectionZone")
 
 	# tear down
+	enemie.shootDetectionZone.player.free()
 	enemie.shootDetectionZone.free()
+	enemie.chaseDetectionZone.player.free()
+	enemie.chaseDetectionZone.free()
 
 
 func test_seek_player_out_of_shoot_zone_and_out_of_chase_zone_IDLE():
@@ -83,8 +88,10 @@ func test_seek_player_in_shoot_zone_and_in_chase_zone_CHASE():
 
 	# arrange
 	enemie.state = Enemie.EnemieState.IDLE
-	enemie.shootDetectionZone = stub_detection_zone(true)
-	enemie.chaseDetectionZone = stub_detection_zone(true)
+	enemie.shootDetectionZone = PlayerDetectionZone.new()
+	enemie.shootDetectionZone.player = Node2D.new()
+	enemie.chaseDetectionZone = PlayerDetectionZone.new()
+	enemie.chaseDetectionZone.player = Node2D.new()
 
 	# act
 	enemie.seek_player()
@@ -92,12 +99,19 @@ func test_seek_player_in_shoot_zone_and_in_chase_zone_CHASE():
 	# assert
 	assert_eq(enemie.state, Enemie.EnemieState.CHASE, "Should activate state CHASE")
 
+	# tear down
+	enemie.shootDetectionZone.player.free()
+	enemie.shootDetectionZone.free()
+	enemie.chaseDetectionZone.player.free()
+	enemie.chaseDetectionZone.free()
+
 
 func test_seek_player_in_shoot_zone_out_of_chase_zone_SHOOT():
 	# arrange
 	enemie.state = Enemie.EnemieState.IDLE
-	enemie.shootDetectionZone = stub_detection_zone(true)
-	enemie.chaseDetectionZone = stub_detection_zone(false)
+	enemie.shootDetectionZone = PlayerDetectionZone.new()
+	enemie.shootDetectionZone.player = Node2D.new()
+	enemie.chaseDetectionZone = PlayerDetectionZone.new()
 
 	# act
 	enemie.seek_player()
@@ -105,13 +119,19 @@ func test_seek_player_in_shoot_zone_out_of_chase_zone_SHOOT():
 	# assert
 	assert_eq(enemie.state, Enemie.EnemieState.SHOOT, "Should activate in state SHOOT")
 
+	# tear down
+	enemie.shootDetectionZone.player.free()
+	enemie.shootDetectionZone.free()
+	enemie.chaseDetectionZone.free()
+
 
 func test_seek_player_no_ranged_weapon_IDLE():
 	# arrange
 	enemie.state = Enemie.EnemieState.IDLE
 	enemie.enemie_resource.ranged_weapon = null
-	enemie.shootDetectionZone = stub_detection_zone(true)
-	enemie.chaseDetectionZone = stub_detection_zone(false)
+	enemie.shootDetectionZone = PlayerDetectionZone.new()
+	enemie.shootDetectionZone.player = Node2D.new()
+	enemie.chaseDetectionZone = PlayerDetectionZone.new()
 
 	# act
 	enemie.seek_player()
@@ -119,8 +139,8 @@ func test_seek_player_no_ranged_weapon_IDLE():
 	# assert
 	assert_eq(enemie.state, Enemie.EnemieState.IDLE, "Should stay in state IDLE")
 
+	# tear down
+	enemie.shootDetectionZone.player.free()
+	enemie.shootDetectionZone.free()
+	enemie.chaseDetectionZone.free()
 
-func stub_detection_zone(value: bool) -> PlayerDetectionZone:
-	var detection_zone = double(PlayerDetectionZone).new()
-	stub(detection_zone, "can_see_player").to_return(value)
-	return detection_zone
